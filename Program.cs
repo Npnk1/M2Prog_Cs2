0a87114f5b7f175df71056b497ab71ffe3b7ff93
Npnk1 <nikita.pasenkov@gmail.com>
2026-01-16 14:29:16 +0100

    ForHerhaling arrays loops foreach

---
+++ Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace ForHerhaling
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Run();
+        }
+
+        static void Run()
+        {
+            // ARRAYS MAKEN
+            double[] prijzen = new double[] { 0.99, 5.60, 10.11, 2.50 };
+
+            string[] artikelen =
+            {
+                "snoepje",
+                "luxe broodje",
+                "lunch menu",
+                "koekje"
+            };
+
+            Formulier[] formulieren = new Formulier[2];
+
+            // FORMULIEREN ARRAY VULLEN
+            formulieren[0] = new Formulier()
+            {
+                Feedback = "prijzig maar lekker",
+                Sterren = 5
+            };
+
+            formulieren[1] = new Formulier()
+            {
+                Feedback = "lunch menu was erg veel",
+                Sterren = 3
+            };
+
+            // FOR LOOP: PRIJZEN + ARTIKELEN
+            for (int i = 0; i < prijzen.Length; i++)
+            {
+                Console.WriteLine(prijzen[i]);
+                Console.WriteLine(artikelen[i]);
+            }
+
+            Console.WriteLine();
+
+            // FOREACH: FORMULIEREN
+            foreach (Formulier formulier in formulieren)
+            {
+                Console.WriteLine(formulier.Feedback);
+                Console.WriteLine(formulier.Sterren);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
+
+
